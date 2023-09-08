09d3bbf0df19ddd4577bd990f2dabf9a7d041e1e
William Bergamin <wbergamin@slack-corp.com>
2023-09-08 14:49:50 -0400

    use condole.table

--- deps.ts
+++ deps.ts
@@ -12,12 +12,6 @@ export {
 export { readLines } from 'https://deno.land/std@0.136.0/io/mod.ts';
 
 export {
-  AsciiAlign,
-  type AsciiData,
-  default as AsciiTable,
-} from 'https://deno.land/x/ascii_table@v0.1.0/mod.ts';
-
-export {
   green,
   red,
   yellow,
--- projectCoverage.ts
+++ projectCoverage.ts
@@ -1,17 +1,19 @@
 import { FileCoverage } from './fileCoverage.ts';
-import { AsciiTable, basename, relative, resolve } from './deps.ts';
+import { basename, relative, resolve } from './deps.ts';
+
+class Row {
+  constructor(public coverage:string, public linesWithoutCoverage: string ) {}
+}
 
 export class ProjectCoverage {
   linesFound = 0;
   linesHit = 0;
 
   fileCoverages = new Set<FileCoverage>();
-  #table = new AsciiTable();
+  #table: Record<string, Row> = {};
   #totalHasBeenAdded = false;
 
-  constructor(public name: string = basename(resolve('.'))) {
-    this.#table.setHeading('File Path', 'Coverage', 'Lines Without Coverage');
-  }
+  constructor(public name: string = basename(resolve('.'))) {}
 
   addFileCoverage(...fcs: FileCoverage[]): typeof this {
     const cwd = Deno.cwd();
@@ -19,11 +21,7 @@ export class ProjectCoverage {
       this.fileCoverages.add(fc);
       this.linesFound += fc.linesFound;
       this.linesHit += fc.linesHit;
-      this.#table.addRow(
-        relative(cwd, fc.name),
-        `${(fc.linesHit / fc.linesFound * 100).toFixed(2)}%`,
-        fc.missingCoverage,
-      );
+      this.#table[relative(cwd, fc.name)] = new Row(`${(fc.linesHit / fc.linesFound * 100).toFixed(2)}%`, fc.missingCoverage);
     }
     return this;
   }
@@ -31,11 +29,8 @@ export class ProjectCoverage {
   printTable() {
     if (!this.#totalHasBeenAdded) {
       this.#totalHasBeenAdded = true;
-      this.#table.addRow(
-        'Totals:',
-        `${(this.linesHit / this.linesFound * 100).toFixed(2)}%`,
-      );
+      this.#table['Totals:'] = new Row(`${(this.linesHit / this.linesFound * 100).toFixed(2)}%`, 'n/a');
     }
-    console.log(this.#table.toString());
+    console.table(this.#table);
   }
 }
